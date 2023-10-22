c4d5721d7a2b02774e7ad5391e85a7fb558c134f
Niles <minhct1997@gmail.com>
2023-10-22 10:45:07 +0700

    Add Spawner

--- Assets/Scripts/Mosquito/MosquitoSpawner.cs
+++ Assets/Scripts/Mosquito/MosquitoSpawner.cs
@@ -6,12 +6,36 @@ public class MosquitoSpawner : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> mosquitos;
+    [SerializeField] GameObject Player;
+    [SerializeField] float OuterCircleSpawnRadius = 20f;
+    [SerializeField] float InnerCircleSpawnRadius = 15f;
 
-    public void SpawnMosquitos(Dictionary<MosquitoType, int> mapData)
+    private void Start()
     {
-
+        StartCoroutine(SpawnByInterval());
     }
 
+    private void OnDrawGizmos()
+    {
+        if (Player == null) return;
+        Gizmos.DrawWireSphere(Player.transform.position, OuterCircleSpawnRadius);
+        Gizmos.DrawWireSphere(Player.transform.position, InnerCircleSpawnRadius);
+    }
+
+    private IEnumerator SpawnByInterval()
+    {
+        while (true)
+        {
+            var randomNextSecond = Random.Range(1, 3);
+            var randomEnemy = mosquitos[Random.Range(0, mosquitos.Count)];
+            var randomCircleRad = (Random.value < 0.5f) ? OuterCircleSpawnRadius : InnerCircleSpawnRadius;
+            var randomPoint = Random.onUnitSphere * randomCircleRad;
+            randomPoint.z = 0;
+
+            Instantiate(randomEnemy, randomPoint, Quaternion.identity);
+            yield return new WaitForSeconds(randomNextSecond);
+        }
+    }
 }
 
 public enum MosquitoType
